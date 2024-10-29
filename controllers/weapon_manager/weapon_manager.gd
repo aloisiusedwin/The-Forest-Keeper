ae27d118e9e0ffbbc07476702d1c0fee127dc2cd
Aloisius Edwin <kp.edwin3007@gmail.com>
2024-10-29 21:22:40 +0700

    organize

--- controllers/weapon_manager/weapon_manager.gd
+++ controllers/weapon_manager/weapon_manager.gd
@@ -1,5 +1,11 @@
+class_name WeaponManager
 extends Node3D
 
+@export var player : CharacterBody3D
+@export var bull_raycast : RayCast3D
+
+@export var view_model_container : Node3D
+#@export var world_model_container : Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
---
+++ controllers/weapon_manager/weapon_resource.gd
@@ -0,0 +1,6 @@
+class_name WeaponResource
+extends Resource
+
+@export var view_model : PackedScene
+@export var world_model : PackedScene
+@export var damage = 20
