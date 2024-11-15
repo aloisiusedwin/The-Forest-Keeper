fbffc88d01230a538a629294e7ca1f98f83670e9
Aloisius Edwin <kp.edwin3007@gmail.com>
2024-11-15 14:42:25 +0700

    notes update

--- Scripts/UI/interact_ray.gd
+++ Scripts/UI/interact_ray.gd
@@ -2,17 +2,20 @@ extends RayCast3D
 
 @onready var prompt = $Prompt
 @onready var note = $"../../../CanvasLayer/Note"
+@onready var label2d = $"../../../CanvasLayer/Note/Label"
 var showNote = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_exception(owner)
 
-func Notes():
+func Notes(isi_notes: String):
 	prompt.hide()
 	if showNote:
 		note.hide()
 		Engine.time_scale = 1
 	else:
+		label2d.text = isi_notes  # Set text di Label2D dengan isi_notes dari Interactable
 		note.show()
 		Engine.time_scale = 0
 	showNote = !showNote
@@ -23,8 +26,9 @@ func Notes():
 func _physics_process(delta: float) -> void:
 	prompt.text = ""
 	if is_colliding():
-		var collider = 	get_collider()
+		var collider = get_collider()
 		if collider is Interactable:
 			prompt.text = collider.prompt_message
 			if Input.is_action_just_pressed("interact"):
-				Notes()
+				collider.interact(self)  # Memanggil interact() dari Interactable
+				Notes(collider.isi_notes)  # Mengirim isi_notes ke fungsi Notes
--- Scripts/UI/interactable.gd
+++ Scripts/UI/interactable.gd
@@ -2,6 +2,7 @@ extends CollisionObject3D
 class_name Interactable
 
 @export var prompt_message = "Press E To Interact"
+@export var isi_notes = ""
 
 func interact(body):
 	print(body.name, " interact with ", name)
