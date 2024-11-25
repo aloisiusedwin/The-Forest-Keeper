5921e32cec09378afb07c9e0e1e2f73fe139753c
Fathan Andi Kartagama <fathan.122140055@student.itera.ac.id>
2024-11-25 13:30:32 +0700

    ulang

--- Scripts/HUD.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-@onready var CurrentAmmo = $HUD/HBoxContainer/MarginContainer/CurrentAmmo
-
-
-func _on_pistol_update_ammo(current,reserve) -> void:
-	CurrentAmmo.set_text(str(current) + " | " + str(reserve))
--- Scripts/interactable.gd
+++
@@ -1,7 +0,0 @@
-extends CollisionObject3D
-class_name Interactable
-
-@export var prompt_message = "Interact"
-
-func interact(body):
-	print(body.name, " interact with ", name)
