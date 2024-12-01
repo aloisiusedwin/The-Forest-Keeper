52d77a4c3317d3761cd7f4b76919c7e48d3f1ec9
Akhdan Arif <akhdan909@gmail.com>
2024-12-01 18:50:17 +0700

    level 3333333

---
+++ Scripts/maps.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@onready var map2_basement = $CanvasLayer/map2_basement
+@onready var map2_lt1 = $CanvasLayer/map2_lt1
+@onready var map2_lt2 = $CanvasLayer/map2_lt2
+@onready var map3_pabrik = $CanvasLayer/map3_pabrik
+@onready var map3_sidequest = $CanvasLayer/map3_sidequest
